2e649de7375ba84d9b530b38a26103ff83bf438e
David Nguyen <hpcsc@users.noreply.github.com>
2023-08-12 00:11:00 +1000

    Setup minimum cli

--- cmd/kz/main.go
+++ cmd/kz/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+	"os"
+)
 
 var Version = "main"
 
 func main() {
-	fmt.Printf("version %s\n", Version)
+	app := &cli.App{
+		Name:     "kz",
+		Usage:    "switch Kubernetes namespace and context using partial name",
+		Version:  Version,
+		Commands: []*cli.Command{},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		color.Red(err.Error())
+		os.Exit(1)
+	}
 }
