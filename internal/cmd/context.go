7355c91869358a5d392713b3340a5b362455e4a3
David Nguyen <hpcsc@users.noreply.github.com>
2023-08-13 21:40:13 +1000

    Add ctx sync subcommand

--- internal/cmd/context.go
+++ internal/cmd/context.go
@@ -1,11 +1,45 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/hpcsc/kz/internal/config"
+	"github.com/hpcsc/kz/internal/kube"
+	"github.com/urfave/cli/v2"
+)
 
 func newContextSubcommand() *cli.Command {
 	return &cli.Command{
-		Name:        "ctx",
-		Aliases:     []string{"context"},
-		Subcommands: []*cli.Command{},
+		Name:    "ctx",
+		Aliases: []string{"context"},
+		Subcommands: []*cli.Command{
+			{
+				Name:   "sync",
+				Usage:  "sync Kubernetes contexts from kube config files",
+				Action: syncContexts,
+			},
+		},
 	}
+}
+
+func syncContexts(ctx *cli.Context) error {
+	c, err := config.LoadFromDefaultLocation()
+	if err != nil {
+		return err
+	}
+
+	contexts, err := kube.ContextsFromConfig()
+	if err != nil {
+		return err
+	}
+
+	c.Contexts = contexts
+
+	if err := config.SaveToDefaultLocation(c); err != nil {
+		return err
+	}
+
+	color.Green(fmt.Sprintf("%d contexts synced", len(contexts)))
+
+	return nil
 }
